20d2ab4dfe71a1bcf10bca8ff27911dfc8110e65
kobe <kobe@vps-MacBook-Air.local>
2017-07-23 08:50:03 -0700

    added console logs

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,5 +1,3 @@
-var inquirer = require('inquirer');
-
 var ClozeCard = function(text, cloze) {
 
 	if (!text.includes(cloze)) {
@@ -23,11 +21,17 @@ var cardTwo = new ClozeCard("This doesn't work", "oops");
 var cardThree = new ClozeCard("'Starry Night' is a famous painting by Van Gogh", "Van Gogh");
 
 console.log(cardOne.fullText);
+console.log("==========================");
 console.log(cardOne.partial);
+console.log("==========================");
 console.log(cardTwo.fullText);
+console.log("==========================");
 console.log(cardTwo.partial);
+console.log("==========================");
 console.log(cardThree.fullText);
+console.log("==========================");
 console.log(cardThree.partial);
+console.log("==========================");
 
 module.exports = ClozeCard;
 
